1e29ea97c8f01ef2482f7de05bf5faf423583399
tunji ty <tytunji29@gmail.com>
2025-09-13 11:10:18 +0100

    fix(auth, jwt): correct secretOrKey assignment in JwtStrategy configuration

--- src/config/jwtstrategy.ts
+++ src/config/jwtstrategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'your_jwt_secret' || 'yourSecretKey',
+      secretOrKey: 'your_jwt_secret',
     });
   }
 
